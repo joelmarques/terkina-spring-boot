0681d41df9390e3c47817dca5baaad218f0a2a98
Joel Marques <bitmarques@gmail.com>
2018-06-20 00:58:57 -0300

    Criacao da arquitetura do projeto em Spring Boot

---
+++ src/main/java/br/com/terkina/TerkinaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package br.com.terkina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerkinaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerkinaSpringBootApplication.class, args);
+	}
+}

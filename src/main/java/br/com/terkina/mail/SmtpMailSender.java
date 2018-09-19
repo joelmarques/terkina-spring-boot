e1fc1316a41ea3b0c649f0a0127644b4eab27f40
Joel Marques <bitmarques@gmail.com>
2018-09-18 23:47:18 -0300

    Configuracao de variaveis de ambiente para envio de emails

--- src/main/java/br/com/terkina/mail/SmtpMailSender.java
+++ src/main/java/br/com/terkina/mail/SmtpMailSender.java
@@ -1,11 +1,9 @@
 package br.com.terkina.mail;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,19 +11,22 @@ public class SmtpMailSender {
 	
 	@Autowired
 	private JavaMailSender javaMailSender;
+
+	@Value("${spring.mail.from}")
+	private String mailFrom;
 	
 	public void send(Message message) {
 		
 		try {
-			
-			MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-			helper.setSubject(message.getSubject());
-			helper.setTo(message.getTo());
-			helper.setText(message.getBody(), false);
-			
-			javaMailSender.send(mimeMessage);
-		} catch (MessagingException e) {
+
+			SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+			simpleMailMessage.setFrom(mailFrom);
+			simpleMailMessage.setTo(message.getTo());
+			simpleMailMessage.setSubject(message.getSubject());
+			simpleMailMessage.setText(message.getBody());
+
+			this.javaMailSender.send(simpleMailMessage);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}		
 	}

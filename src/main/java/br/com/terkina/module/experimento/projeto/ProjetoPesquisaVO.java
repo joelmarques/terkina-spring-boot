36bdee0ea89b8f9337b1345e4f4df3fa254d5b89
Joel Marques <bitmarques@gmail.com>
2018-10-31 19:43:01 -0200

    Refactoring para o padrao Spring Boot

--- src/main/java/br/com/terkina/module/experimento/projeto/ProjetoPesquisaVO.java
+++ src/main/java/br/com/terkina/module/experimento/projeto/ProjetoPesquisaVO.java
@@ -1,13 +1,8 @@
 package br.com.terkina.module.experimento.projeto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@Getter @Setter
-public class ProjetoPesquisaVO {
-	private Long id;	
-	private String titulo;	
-	private String tipoPesquisa;
+public interface ProjetoPesquisaVO {
+	
+	Long getId();
+	String getTitulo();
+	String getTipoPesquisa();
 }
